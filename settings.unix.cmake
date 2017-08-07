60620f6466273f845fdad7434f46a4a4b159f496
z80 <bashkirov.sergey@gmail.com>
2017-08-06 21:53:25 -0700

    Started simple test application.

---
+++ settings.unix.cmake
@@ -0,0 +1,7 @@
+
+set( OpenCV_DIR "~/projects/opencv.git/.install.debug/share/OpenCV" )
+
+set( VTK_DIR "~/projects/vtk/lib/lib/cmake/vtk-7.0" )
+#set( VTK_DIR "~/projects/vtk/lib/" )
+set( VTK_RENDERING_BACKEND OpenGL )
+
---
+++ tests/slam/src/main.cpp
@@ -0,0 +1,53 @@
+
+#include<iostream>
+#include<algorithm>
+#include<fstream>
+#include<chrono>
+
+#include<opencv2/core/core.hpp>
+#include<opencv2/imgproc.hpp>
+#include<opencv2/highgui.hpp>
+#include<opencv2/videoio.hpp>
+#include<opencv2/features2d.hpp>
+
+#include<System.h>
+
+using namespace std;
+
+
+
+int main(int argc, char **argv)
+{
+
+    // Create SLAM system. It initializes all system threads and gets ready to process frames.
+    ORB_SLAM2::System SLAM( "./ORBvoc.txt", "./settings.yaml", ORB_SLAM2::System::MONOCULAR, true );
+
+    cv::Mat img;
+    cv::VideoCapture capture;
+    capture.open( 0 );
+
+    int i=0;
+    for (;;)
+    {
+        capture >> img;
+        //std::chrono::monotonic_clock::time_point t1 = std::chrono::monotonic_clock::now();
+
+        double tframe = static_cast<double>(i++);
+        SLAM.TrackMonocular( img, tframe );
+
+        uchar key = (uchar)cv::waitKey( 0 );
+        if ( key == "q" )
+            break;
+    }
+
+    //im = cv::imread( vstrImageFilenames[ni],CV_LOAD_IMAGE_UNCHANGED );
+
+
+    // Stop all threads
+    SLAM.Shutdown();
+
+
+
+    return 0;
+}
+
