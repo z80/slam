a4f9eddb54953bd7b0f440c39cb02ba5f319b9e0
Sergey Bashkirov <sbashkirov@sbashkirov-linux>
2017-09-12 12:28:52 -0700

    Doesn't work.

--- vo/libvo/src/vo.cpp
+++ vo/libvo/src/vo.cpp
@@ -30,9 +30,12 @@ public:
     bool loadCameraParams( cv::Mat & cameraMatrix,
                            cv::Mat & distCoefs,
                            const char * fileName = "./cameraParameters.xml" );
-    bool detectFeatures( cv::Ptr<cv::Feature2D> detector, const cv::Mat & image, std::vector<cv::Point2f> & pts );
+    bool detectFeatures( cv::Ptr<cv::Feature2D> detector, const cv::Mat & image, std::vector<cv::Point2f> & pts, cv::Mat & descriptors );
     bool trackOpticalFlow( const cv::Mat & imgA, std::vector<cv::Point2f> & pointsA,
                            const cv::Mat & imgB, std::vector<cv::Point2f> & pointsB );
+    bool matchFeatures( cv::Ptr<cv::DescriptorMatcher> matcher,
+                        std::vector<cv::Point2f> & pointsA, cv::Mat featuresA,
+                        std::vector<cv::Point2f> & pointsB, cv::Mat featuresB );
     bool computePoseDisplacement( std::vector<cv::Point2f> & pointsA,
                                   std::vector<cv::Point2f> & pointsB,
                                   const cv::Mat & cameraMatrix );
@@ -83,13 +86,16 @@ bool Vo::PD::loadCameraParams( cv::Mat & cameraMatrix,
     return true;
 }
 
-bool Vo::PD::detectFeatures( cv::Ptr<cv::Feature2D> detector, const cv::Mat & image, std::vector<cv::Point2f> & pts )
+bool Vo::PD::detectFeatures( cv::Ptr<cv::Feature2D> detector, const cv::Mat & image, std::vector<cv::Point2f> & pts, cv::Mat & descriptors )
 {
     std::vector<cv::KeyPoint> kp;
     //int FAST_th = 20;
     //bool nonmaxSuppression = true;
     //cv::FAST( image, kp, FAST_th, nonmaxSuppression );
+
     detector->detect( image, kp );
+    //detector->detectAndCompute( image, cv::noArray(), kp, descriptors );
+
     cv::KeyPoint::convert( kp, pts, std::vector<int>() );
     return true;
 }
@@ -129,13 +135,38 @@ bool Vo::PD::trackOpticalFlow( const cv::Mat & imgA, std::vector<cv::Point2f> & 
     return true;
 }
 
+bool Vo::PD::matchFeatures( cv::Ptr<cv::DescriptorMatcher> matcher,
+                            std::vector<cv::Point2f> & pointsA, cv::Mat descsA,
+                            std::vector<cv::Point2f> & pointsB, cv::Mat descsB )
+{
+    const double nnMatchRatio = 0.8;
+    std::vector<cv::Point2f> matchedA, matchedB;
+    std::vector< std::vector<cv::DMatch> > matches;
+
+    matcher->knnMatch( descsA, descsB, matches, 2 );
+
+    for ( unsigned i=0; i<matches.size(); i++ )
+    {
+        if ( matches[i][0].distance < nnMatchRatio * matches[i][1].distance )
+        {
+            matchedA.push_back( pointsA[ matches[i][0].queryIdx ] );
+            matchedB.push_back( pointsB[ matches[i][0].trainIdx ] );
+        }
+    }
+    pointsA = matchedA;
+    pointsB = matchedB;
+
+    return true;
+}
+
+
 bool Vo::PD::computePoseDisplacement( std::vector<cv::Point2f> & pointsA,
                                       std::vector<cv::Point2f> & pointsB,
                                       const cv::Mat & cameraMatrix )
 {
     cv::Mat E, r, t, mask;
     std::vector<uchar> status;
-    E = cv::findEssentialMat( pointsA, pointsB, cameraMatrix, cv::RANSAC, 0.999, 1.0, mask );
+    E = cv::findEssentialMat( pointsA, pointsB, cameraMatrix, cv::RANSAC, 0.99, 1.0, mask );
     int res = cv::recoverPose( E, pointsA, pointsB, cameraMatrix, r, t, status );
 
     t       = this->R * t;
@@ -143,7 +174,8 @@ bool Vo::PD::computePoseDisplacement( std::vector<cv::Point2f> & pointsA,
     //cv::Rodrigues( r, R );
 
     std::cout << "res: " << res << std::endl;
-    //std::cout << t << std::endl;
+    std::cout << "t: "   << t << std::endl;
+    std::cout << "R: "   << r << std::endl;
 
     if ( res < 20 )
         return false;
@@ -155,9 +187,9 @@ bool Vo::PD::computePoseDisplacement( std::vector<cv::Point2f> & pointsA,
     double z  = (R.at<double>(1,0) - R.at<double>(0,1)) / w4;
 
     boost::mutex::scoped_lock lock( this->dataMutex );
-        this->x += t.at<double>( 0, 0 );
-        this->y += t.at<double>( 1, 0 );
-        this->z += t.at<double>( 2, 0 );
+        this->x += t.at<double>( 0, 0 ) * 0.0;
+        this->y += t.at<double>( 1, 0 ) * 0.0;
+        this->z += t.at<double>( 2, 0 ) * 0.0;
         this->qw = w;
         this->qx = x;
         this->qy = y;
@@ -190,8 +222,10 @@ void Vo::PD::process()
     // For holding previous image.
     cv::Mat prevImage;
     std::vector<cv::Point2f> prevFeaturePoints;
+    cv::Mat prevDescriptors;
     // For current image.
-    std::vector<cv::Point2f> featurePoints;
+    std::vector<cv::Point2f> featurePoints, selectedFeaturePoints;
+    cv::Mat descriptors, selectedDescriptors;
 
     cv::Mat rawImage;
     cv::Mat greyImage;
@@ -200,6 +234,7 @@ void Vo::PD::process()
     this->R = cv::Mat::eye( 3, 3, CV_64F );
     cv::Ptr<cv::ORB> detector = cv::ORB::create( 500 );
     //cv::Ptr<cv::Feature2D> detector = cv::FAST();
+    cv::Ptr<cv::DescriptorMatcher> matcher = cv::DescriptorMatcher::create( "BruteForce-Hamming" );
 
     do {
         inputCapture >> rawImage;
@@ -210,7 +245,9 @@ void Vo::PD::process()
         //      Step 1.
         // Detect feature points.
         // *******************
-        this->detectFeatures( detector, greyImage, featurePoints );
+        this->detectFeatures( detector, greyImage, featurePoints, descriptors );
+        selectedFeaturePoints = featurePoints;
+        selectedDescriptors   = descriptors.clone();
 
         if ( ( greyImage.rows > 0 ) && ( prevImage.rows > 0) )
         {
@@ -221,8 +258,10 @@ void Vo::PD::process()
             if ( featurePoints.size() > 20 )
                 trackOpticalFlow( greyImage, featurePoints,
                                   prevImage, prevFeaturePoints );
+                //matchFeatures( matcher, prevFeaturePoints,     prevDescriptors,
+                //                        selectedFeaturePoints, selectedDescriptors );
             // If at least some points are tracked.
-            if ( ( prevFeaturePoints.size() > 20 ) && ( featurePoints.size() > 20 ) )
+            if ( ( prevFeaturePoints.size() > 20 ) && ( selectedFeaturePoints.size() > 20 ) )
                 // *******************
                 //       Step 3.
                 // Retrieve rotation and translation.
@@ -231,7 +270,9 @@ void Vo::PD::process()
         }
         // Loop.
         // Save previous image and feature points.
-        prevImage = greyImage.clone();
+        prevImage         = greyImage.clone();
+        prevFeaturePoints = featurePoints;
+        prevDescriptors   = descriptors;
 
     } while ( !terminated );
 }
